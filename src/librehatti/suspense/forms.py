bf225e52097027c656217e666c18189aca2f3d18
ChanKaur <ChanKaur@github.com>
2014-09-02 22:33:54 +0530

    LibreHatti

---
+++ src/librehatti/suspense/forms.py
@@ -0,0 +1,36 @@
+from django.forms import ModelForm, TextInput
+from models import SuspenseClearance
+from models import TaDa
+from models import SuspenseOrder
+from models import Staff
+from models import ProgrammeLetter
+from django import forms
+
+# Create your forms here.
+class Clearance_form(ModelForm):
+    class Meta:
+        model = SuspenseClearance
+        fields = ['work_charge','labour_charge','car_taxi_charge',
+                  'boring_charge_external','boring_charge_internal',
+		  'lab_testing_staff','field_testing_staff','test_date']
+
+
+class SuspenseForm(ModelForm):
+    class Meta:
+        model = SuspenseOrder
+        exclude = ('is_cleared',)
+      
+class TaDaSearch(forms.Form):
+    ref_no = forms.ModelChoiceField(queryset= SuspenseOrder.objects.all())
+
+class TaDaForm(ModelForm):
+    class Meta:
+        model = TaDa
+        exclude = ('',)
+
+
+class LetterForm(ModelForm):
+    class Meta:
+        model=ProgrammeLetter
+        fieds=[' address', 'clientName',' clientContact',' subject','staff','site_venue','site_date']
+
